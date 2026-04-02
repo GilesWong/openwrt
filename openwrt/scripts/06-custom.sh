699996d06a3697704e143dfac1320547b3150df2
Giles <iwg@live.com>
2026-04-02 18:49:58 +0800

    build: add vlmcsd and natfrp, remove aria2 and ariang
    - Enable luci-app-vlmcsd (KMS activation service) from mchome/openwrt-vlmcsd
    - Add luci-app-natfrp (SakuraFrp) via prebuilt IPK with first-boot install
    - Disable luci-app-aria2 and ariang from custom config

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -10,3 +10,23 @@ git clone https://$github/sbwml/luci-app-openlist2 package/new/openlist
 # lrzsz - add patched package
 rm -rf feeds/packages/utils/lrzsz
 git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
+
+# vlmcsd - add new package
+git clone https://$github/mchome/openwrt-vlmcsd package/new/vlmcsd
+git clone https://$github/mchome/luci-app-vlmcsd package/new/luci-app-vlmcsd
+
+# natfrp - download prebuilt ipk, inject into rootfs for first-boot install
+mkdir -p files/natfrp
+if [ "$platform" = "x86_64" ]; then
+    natfrp_arch="amd64"
+elif [ "$platform" = "rk3568" ]; then
+    natfrp_arch="arm64"
+elif [ "$platform" = "rk3399" ]; then
+    natfrp_arch="armv7"
+fi
+natfrp_ver=$(curl -s https://nya.globalslb.net/natfrp/client/launcher-openwrt/ | grep -oE '[0-9]+\.[0-9]+\.[0-9]+(?=/)' | sort -V | tail -n 1)
+natfrp_ver=${natfrp_ver:-"3.1.7"}
+curl -Lso files/natfrp/luci-app-natfrp.ipk "https://nya.globalslb.net/natfrp/client/launcher-openwrt/$natfrp_ver/luci-app-natfrp_${natfrp_arch}.ipk"
+mkdir -p files/etc/init.d
+curl -so files/etc/init.d/natfrp-install https://$mirror/openwrt/files/etc/init.d/natfrp-install
+chmod 755 files/etc/init.d/natfrp-install
